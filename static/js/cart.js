7c3c103a1e043bffacd327c4dfeb86f8ec7f7cd2
joseph-karanja <josephgithinji2000@gmail.com>
2022-07-19 12:53:34 +0300

    completed paypal integration

--- static/js/cart.js
+++ static/js/cart.js
@@ -9,7 +9,7 @@ for(var i = 0; i < updateBtns.length; i++){
 
     console.log('USER:', user)
     if(user === 'AnonymousUser'){
-      console.log('Not logged in')
+      addCookieItem(productId, action)
     }else{
       updateUserOrder(productId, action)
     }
@@ -22,8 +22,7 @@ function addCookieItem(productId, action){
 
 	if (action == 'add'){
 		if (cart[productId] == undefined){
-		cart[productId] = {'quantity':1}
-
+		    cart[productId] = {'quantity':1}
 		}else{
 			cart[productId]['quantity'] += 1
 		}
